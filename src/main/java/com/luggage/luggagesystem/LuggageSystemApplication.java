c041108982cdf1a951bcc8fc8c6dbae55da93c67
Lng <924106840503@njust.edu.cn>
2026-08-30 15:56:34 +0800

    初始化

---
+++ src/main/java/com/luggage/luggagesystem/LuggageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.luggage.luggagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuggageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuggageSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luggage/luggagesystem/LuggageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luggage.luggagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuggageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
